1a016adc9170350369ef464ec4078ae2c0a7bc6a
wj008 <26029682@qq.com>
2018-04-11 05:31:35 +0800

    调整PageList 优化和非优化区分

--- core/PageList.php
+++ core/PageList.php
@@ -82,7 +82,6 @@ class PageList
 
     /**
      * 获取 分页信息数据
-     * @return array <br/>
      * array(<br/>
      * 'page_key'      ：分页参数名称 如果 page,<br/>
      * 'query'         ：本页所附带的连接 即 $_SERVER['REQUEST_URI'] <br/>例如 cid=3&keyword=abc&page=3<br/>
@@ -92,6 +91,8 @@ class PageList
      * 'records_count' ：记录数,<br/>
      * 'size'          ：分页尺度，即按多少条记录为一页,<br/>
      * )
+     * @return array|null
+     * @throws \Exception
      */
     public function getInfo()
     {
@@ -105,13 +106,17 @@ class PageList
             $query = substr($url, $Idx + 1);
         }
         if ($this->recordsCount == -1) {
-            if (strripos($this->sql, ' from ') === stripos($this->sql, ' from ')) {
-                $sql = preg_replace('@^select\s+(distinct\s+[a-z][a-z0-9]+\s*,)?(.*)\s+from\s+@', 'select $1count(1) from ', $this->sql);
-                $row = DB::getRow($sql, $this->args, PDO::FETCH_NUM);
+            if ($this->selector) {
+                $this->recordsCount = $this->selector->getCount();
             } else {
-                $row = DB::getRow('select count(1) from (' . $this->sql . ') MyTempTable', $this->args, PDO::FETCH_NUM);
+                if (strripos($this->sql, ' from ') === stripos($this->sql, ' from ')) {
+                    $sql = preg_replace('@^select\s+(distinct\s+[a-z][a-z0-9]+\s*,)?(.*)\s+from\s+@', 'select $1count(1) from ', $this->sql);
+                    $row = DB::getRow($sql, $this->args, PDO::FETCH_NUM);
+                } else {
+                    $row = DB::getRow('select count(1) from (' . $this->sql . ') MyTempTable', $this->args, PDO::FETCH_NUM);
+                }
+                $this->recordsCount = $row[0];
             }
-            $this->recordsCount = $row[0];
         }
         if ($this->onlyCount == -1 || $this->onlyCount > $this->recordsCount) {
             $this->onlyCount = $this->recordsCount;
@@ -141,11 +146,12 @@ class PageList
     }
 
     /**
-     * 获取 分页后的记录集数据
+     *  获取 分页后的记录集数据
      * @param null $fetch_style
      * @param null $fetch_argument
      * @param array|null $ctor_args
-     * @return mixed
+     * @return array
+     * @throws \Exception
      */
     public function getList($fetch_style = null, $fetch_argument = null, array $ctor_args = null)
     {
@@ -163,10 +169,12 @@ class PageList
     }
 
     /**
+     * 获取所有数据
      * @param null $fetch_style
      * @param null $fetch_argument
      * @param array|null $ctor_args
-     * @return mixed
+     * @return array
+     * @throws \Exception
      */
     public function getAll($fetch_style = null, $fetch_argument = null, array $ctor_args = null)
     {
