8aeb10e4727f6af759337c4b5b8d90f485a73a31
sphynx <327388905@qq.com>
2020-04-22 13:50:30 +0800

    修复创建和添加字段默认值导致不能处理的问题

--- core/Mysql.php
+++ core/Mysql.php
@@ -886,7 +886,9 @@ class Mysql
                 $sql .= $type;
                 break;
         }
-        $sql .= ' DEFAULT ' . Mysql::escape($def);
+        if (!in_array(strtoupper($type), ['BLOB', 'TEXT', 'GEOMETRY', 'JSON'])) {
+            $sql .= ' DEFAULT ' . Mysql::escape($def);
+        }
         if ($comment) {
             $sql .= ' COMMENT ' . Mysql::escape($comment);
         }
@@ -945,7 +947,9 @@ class Mysql
                 $sql .= $type;
                 break;
         }
-        $sql .= ' DEFAULT ' . Mysql::escape($def);
+        if (!in_array(strtoupper($type), ['BLOB', 'TEXT', 'GEOMETRY', 'JSON'])) {
+            $sql .= ' DEFAULT ' . Mysql::escape($def);
+        }
         if ($comment) {
             $sql .= ' COMMENT ' . Mysql::escape($comment);
         }
