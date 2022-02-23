6552dbb6464cdb1371bddeb8f6602a283b3b27a3
wj008 <26029682@qq.com>
2022-02-24 00:43:40 +0800

    优化配置文件

--- widget/Time.php
+++ widget/Time.php
@@ -11,8 +11,8 @@ use beacon\core\Validator;
 #[\Attribute]
 class Time extends Field
 {
-    protected array $_attrs=[
-        'class'=>'form-inp select',
+    protected array $_attrs = [
+        'class' => 'form-inp select',
     ];
 
     /**
@@ -22,37 +22,37 @@ class Time extends Field
      */
     protected function code(array $attrs = []): string
     {
-        $values=explode(':',$attrs['value']);
-        $vHour=intval($values[0]??0);
-        $vMinute=intval($values[1]??0);
-        $code=[];
+        $values = explode(':', $attrs['value']);
+        $vHour = intval($values[0] ?? 0);
+        $vMinute = intval($values[1] ?? 0);
+        $code = [];
         $hCode = [];
-        $name=$attrs['name'];
-        for($i=0;$i<24;$i++){
-            $option=['value'=>$i,'text'=>str_pad($i,2,'0',STR_PAD_LEFT)];
-            $text=str_pad($i,2,'0',STR_PAD_LEFT).'时';
+        $name = $attrs['name'];
+        for ($i = 0; $i < 24; $i++) {
+            $option = ['value' => $i, 'text' => str_pad($i, 2, '0', STR_PAD_LEFT)];
+            $text = str_pad($i, 2, '0', STR_PAD_LEFT) . '时';
             if (strval($vHour) == strval($i)) {
                 $option['selected'] = 'selected';
             }
-            $hCode[]= static::makeTag('option', ['attrs' => $option, 'text' =>$text, 'filter' => false]);
+            $hCode[] = static::makeTag('option', ['attrs' => $option, 'text' => $text, 'filter' => false]);
         }
         $optCode = "\n" . join("\n", $hCode) . "\n";
-        $attrs['name']=$name.'_hour';
-        $code[]= static::makeTag('select', ['attrs' => $attrs, 'exclude' => ['value'], 'code' => $optCode]);
+        $attrs['name'] = $name . '_hour';
+        $code[] = static::makeTag('select', ['attrs' => $attrs, 'exclude' => ['value'], 'code' => $optCode]);
 
-        $mCode=[];
-        for($i=0;$i<60;$i++){
-            $option=['value'=>$i,'text'=>str_pad($i,2,'0',STR_PAD_LEFT)];
-            $text=str_pad($i,2,'0',STR_PAD_LEFT).'分';
+        $mCode = [];
+        for ($i = 0; $i < 60; $i++) {
+            $option = ['value' => $i, 'text' => str_pad($i, 2, '0', STR_PAD_LEFT)];
+            $text = str_pad($i, 2, '0', STR_PAD_LEFT) . '分';
             if (strval($vMinute) == strval($i)) {
                 $option['selected'] = 'selected';
             }
-            $mCode[]= static::makeTag('option', ['attrs' => $option, 'text' =>$text, 'filter' => false]);
+            $mCode[] = static::makeTag('option', ['attrs' => $option, 'text' => $text, 'filter' => false]);
         }
         $optCode = "\n" . join("\n", $mCode) . "\n";
-        $attrs['name']=$name.'_minute';
-        $code[]= static::makeTag('select', ['attrs' => $attrs, 'exclude' => ['value'], 'code' => $optCode]);
-        return join("\n",$code);
+        $attrs['name'] = $name . '_minute';
+        $code[] = static::makeTag('select', ['attrs' => $attrs, 'exclude' => ['value'], 'code' => $optCode]);
+        return join("\n", $code);
     }
 
     /**
@@ -62,23 +62,23 @@ class Time extends Field
      */
     public function fromParam(array $param = []): int|string
     {
-        $name=$this->boxName;
-        $hName=$name.'_hour';
-        $mName=$name.'_minute';
+        $name = $this->boxName;
+        $hName = $name . '_hour';
+        $mName = $name . '_minute';
         $hValue = Request::lookType($param, $hName, 'string', '');
         $mValue = Request::lookType($param, $mName, 'string', '');
-        if (empty($hValue) || empty($mValue) || !Validator::testInteger($hValue)||!Validator::testInteger($mValue)) {
+        if (strval($hValue) === '' || strval($mValue) === '' || !Validator::testInteger($hValue) || !Validator::testInteger($mValue)) {
             return '';
         }
-        if($hValue<0||$hValue>24){
+        if (intval($hValue) < 0 || intval($hValue) > 24) {
             return '';
         }
-        if($mValue<0||$mValue>60){
+        if (intval($mValue) < 0 || intval($mValue) > 60) {
             return '';
         }
-        $hValue= str_pad($hValue,2,'0',STR_PAD_LEFT);
-        $mValue= str_pad($mValue,2,'0',STR_PAD_LEFT);
-        return $hValue.':'.$mValue.':00';
+        $hValue = str_pad($hValue, 2, '0', STR_PAD_LEFT);
+        $mValue = str_pad($mValue, 2, '0', STR_PAD_LEFT);
+        return $hValue . ':' . $mValue . ':00';
     }
 
     /**
